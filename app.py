636347f3d98cefbb71f0d6e5f5e17120a9b9dcc1
peter <peter.26cast@gmail.com>
2023-02-15 20:10:53 -0500

    /

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension 
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adoption_agency'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = 'lol_secret_key'
+
+app.app_context().push()
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,18 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primarykey=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.text, nullable=False)
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
